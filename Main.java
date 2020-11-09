178012cea18a9bc526d8ab3a54222e7035d9c671
Leonardo Rocha <leoheiro@hotmail.com>
2020-11-09 12:12:01 -0300

    Lendo arquivo .learn

--- Main.java
+++ Main.java
@@ -1,15 +1,31 @@
-
+import java.util.Scanner;
+import java.io.File;
 
 public class Main{
 
     public static void main(String[] args){
-        
+        String path;
         if (args.length > 0){
-            String path = args[0];
+            path = args[0];
         } else {
             System.out.println("Arquivo nao especificado.");
+            return;
         }
 
+        try {
+                File file = new File(path);
+                Scanner input = new Scanner(file);
+                while (input.hasNextLine()) {
+                    String line = input.nextLine();
+                    System.out.println(line);
+            }
+
+            input.close();
+            
+            } catch (Exception e) {
+                System.out.println("Nao foi possivel abrir o arquivo " + path);
+                e.printStackTrace();
+            }
 
     }
 
